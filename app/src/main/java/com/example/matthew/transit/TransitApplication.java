b25fa47e7234bd97128dcbd4319ceb9079c74d2b
Matthew Klein <mklein994@gmail.com>
2016-04-12 11:16:53 -0500

    Add Realm configuration

---
+++ app/src/main/java/com/example/matthew/transit/TransitApplication.java
@@ -0,0 +1,19 @@
+package com.example.matthew.transit;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by matthew on 11/04/16.
+ */
+public class TransitApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        RealmConfiguration config = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
